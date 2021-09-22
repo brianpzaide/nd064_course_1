ee3375768682e8e233e6e1643913bbccf0d94877
brianpzaide <brianpzaide@gmail.com>
2021-09-22 08:31:00 +0000

    -

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -72,12 +72,12 @@ def create():
     return render_template('create.html')
 
 
-@app.route("/healthz")
+@app.route('/healthz')
 def healthcheck():
   response = app.response_class(response=json.dumps({"result": "OK - healthy"}), status=200, mimetype="application/json")
   return response
 
-@app.route("/metrics")
+@app.route('/metrics')
 def metrics():
   connection = sqlite3.connect('database.db')
   cur = connection.cursor()
